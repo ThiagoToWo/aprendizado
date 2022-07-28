f475ba6eac7ceb16847130d917809164c27e52c0
ThiagoToWo <towo497@gmail.com>
2022-07-28 15:33:46 -0300

    primeiro commit

---
+++ c/10_eratostenes/eratostenes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void main() {
+    int max;
+    int amplitude;
+
+    printf("Insira o limite de teste: ");
+    scanf("%d", &max);
+    printf("Insira a amplitude de distribuicao: ");
+    scanf("%d", &amplitude);
+
+    int primos[max + 1];
+
+    primos[0] = 0;
+    primos[1] = 0;
+
+    for (int i = 2; i <= max; i++) {
+        primos[i] = 1;
+    }
+
+    for (int i = 2; i * i <= max; i++) {
+        for (int j = 2 * i; j <= max; j += i) {
+            primos[j] = 0;
+        }
+    }
+
+    for (int i = 2; i <= max; i++) {
+        if (primos[i]) {
+            printf("%d ", i);
+        }
+        
+        if (i % amplitude == 0) {
+            printf("\n");
+        }
+    }
+}
