85af667d8dc6a009e86d52944c245c634f89d192
ThiagoToWo <towo497@gmail.com>
2023-10-02 00:01:54 -0300

    separação de arquivos na pasta de modulos

--- python/15_ordenacao/ordenacao.py
+++
@@ -1,7 +0,0 @@
-def ordenar(vetor, n):
-    for i in range(1, n):
-        for j in range(n - i):
-            if vetor[j] > vetor[j + 1]:
-                temp = vetor[j]
-                vetor[j] = vetor[j + 1]
-                vetor[j + 1] = temp
