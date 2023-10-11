e68b540e7521d9442b799b51cd3bef35f86f42bd
ThiagoToWo <towo497@gmail.com>
2023-10-11 00:21:12 -0300

    primeiro commit

---
+++ c/compile_execute.bat
@@ -0,0 +1,137 @@
+@echo off
+
+rem apague a marcação de comentário (rem) para compilar o código
+
+rem gcc 01_ola_mundo/ola_mundo1.c -o 01_ola_mundo/ola_mundo1
+rem gcc 01_ola_mundo/ola_mundo2.c -o 01_ola_mundo/ola_mundo2
+rem gcc 01_ola_mundo/ola_mundo3.c -o 01_ola_mundo/ola_mundo3
+rem gcc 01_ola_mundo/ola_mundo4.c -o 01_ola_mundo/ola_mundo4
+rem gcc 02_calculadora/calculadora.c -o 02_calculadora/calculadora
+rem gcc 03_bissexto/bissexto.c -o 03_bissexto/bissexto
+rem gcc 04_bahskara/bahskara.c -o 04_bahskara/bahskara
+rem gcc 05_media/media1.c -o 05_media/media1
+rem gcc 05_media/media2.c -o 05_media/media2
+rem gcc 06_adivinhe/adivinhe.c -o 06_adivinhe/adivinhe
+rem gcc 07_classificacao/classificacao.c -o 07_classificacao/classificacao
+rem gcc 08_maximo/maximo.c -o 08_maximo/maximo
+rem gcc 09_labirinto/labirinto.c -o 09_labirinto/labirinto
+rem gcc 10_eratostenes/eratostenes.c -o 10_eratostenes/eratostenes
+rem gcc 11_tebela/tebela.c -o 11_tebela/tebela
+rem gcc 12_contagem/contagem.c -o 12_contagem/contagem
+rem gcc 13_subtexto/subtexto.c -o 13_subtexto/subtexto
+rem gcc 14_tokens/tokens.c -o 14_tokens/tokens
+rem gcc 15_ordenacao/ordenacao.h 15_ordenacao/teste_ordenacao.c -o 15_ordenacao/teste_ordenacao
+rem gcc 16_busca/busca.h 16_busca/teste_busca.c -o 16_busca/teste_busca
+rem gcc 17_estatistica/estatistica.h 17_estatistica/teste_estatistica1.c -o 17_estatistica/teste_estatistica1
+rem gcc 17_estatistica/estatistica.h 17_estatistica/teste_estatistica2.c -o 17_estatistica/teste_estatistica2
+rem gcc 18_lista/lista.h 18_lista/teste_lista.c -o 18_lista/teste_lista
+rem gcc 19_pilha/pilha.h 19_pilha/teste_pilha.c -o 19_pilha/teste_pilha
+rem gcc 20_fila/fila.h 20_fila/teste_fila.c -o 20_fila/teste_fila
+rem gcc 21_arvore/arvore.h 21_arvore/teste_arvore.c -o 21_arvore/teste_arvore
+rem gcc 22_crud/lista_crud.h 22_crud/crud.c -o 22_crud/crud
+
+rem apague a marcação de comentário (rem) e o trecho "../" após cd para executar o código
+
+rem cd 01_ola_mundo
+rem .\ola_mundo1
+rem echo.
+rem pause
+rem .\ola_mundo2
+rem echo.
+rem pause
+rem .\ola_mundo3
+rem echo.
+rem pause
+rem .\ola_mundo4
+echo.
+rem pause
+rem cd ../02_calculadora
+rem .\calculadora
+rem echo.
+rem pause
+rem cd ../03_bissexto
+rem .\bissexto
+rem echo.
+rem pause
+rem cd ../04_bahskara
+rem .\bahskara
+rem echo.
+rem pause
+rem cd ../05_media
+rem .\media1
+rem echo.
+rem pause
+rem .\media2
+rem echo.
+rem pause
+rem cd ../06_adivinhe
+rem .\adivinhe
+rem echo.
+rem pause
+rem cd ../07_classificacao
+rem .\classificacao
+rem echo.
+rem pause
+rem cd ../08_maximo
+rem .\maximo
+rem echo.
+rem pause
+rem cd ../09_labirinto
+rem .\labirinto
+rem echo.
+rem pause
+rem cd ../10_eratostenes
+rem .\eratostenes
+rem echo.
+rem pause
+rem cd ../11_tabela
+rem .\tabela
+rem echo.
+rem pause
+rem cd ../12_contagem
+rem .\contagem
+rem echo.
+rem pause
+rem cd ../13_subtexto
+rem .\subtexto
+rem echo.
+rem pause
+rem cd ../14_tokens
+rem .\tokens
+rem echo.
+rem pause
+rem cd ../15_ordenacao
+rem .\teste_ordenacao
+rem echo.
+rem pause
+rem cd ../16_busca
+rem .\teste_busca
+rem echo.
+rem pause
+rem cd ../17_estatistica
+rem .\teste_estatistica1
+rem echo.
+rem pause
+rem .\teste_estatistica2
+rem echo.
+rem pause
+rem cd ../18_lista
+rem .\teste_lista
+rem echo.
+rem pause
+rem cd ../19_pilha
+rem .\teste_pilha
+rem echo.
+rem pause
+rem cd ../20_fila
+rem .\teste_fila
+rem echo.
+rem pause
+rem cd ../21_arvore
+rem .\teste_arvore
+rem echo.
+rem pause
+rem cd ../22_crud
+rem .\crud
+rem echo.
+rem pause
